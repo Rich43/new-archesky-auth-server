1bb8fb984cad808578ef601ce9c284c9ca6d2d1f
Richard Ward <pynguins@outlook.com>
2021-04-23 21:14:36 +0100

    Initial commit

---
+++ src/main/java/com/pynguins/archesky/archeskyauthserver/ArcheskyAuthServerApplication.java
@@ -0,0 +1,11 @@
+package com.pynguins.archesky.archeskyauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcheskyAuthServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArcheskyAuthServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pynguins/archesky/archeskyauthserver/ArcheskyAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pynguins.archesky.archeskyauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcheskyAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
