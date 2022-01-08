01e02ebe351689c14b442ac74ec24c985c6e142d
Richard Ward <richies@gmail.com>
2022-01-08 14:16:55 +0000

    Migrate to a post request

--- src/main/java/com/pynguins/archesky/archeskyauthserver/AuthController.java
+++ src/main/java/com/pynguins/archesky/archeskyauthserver/AuthController.java
@@ -6,7 +6,10 @@ import com.pynguins.archesky.archeskyauthserver.dto.Role;
 import com.pynguins.archesky.archeskyauthserver.dto.Token;
 import com.pynguins.archesky.archeskyauthserver.service.TokenService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.naming.AuthenticationException;
 import java.net.MalformedURLException;
@@ -18,6 +21,24 @@ import static org.springframework.http.HttpStatus.FOUND;
 
 @RestController
 public class AuthController {
+    private static class CheckTokenRequestBody {
+        private final String token;
+        private final String hostname;
+
+        private CheckTokenRequestBody(String token, String hostname) {
+            this.token = token;
+            this.hostname = hostname;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public String getHostname() {
+            return hostname;
+        }
+    }
+
     private final TokenService tokenService;
 
     public AuthController(TokenService tokenService) {
@@ -29,9 +50,12 @@ public class AuthController {
         return ResponseEntity.status(FOUND).location(new URI("/swagger-ui/")).build();
     }
 
-    @RequestMapping(value = "/auth", method = RequestMethod.GET)
-    public Token checkToken(@RequestParam String token, @RequestHeader String hostname) throws MalformedURLException, AuthenticationException, JwkException {
-        final DecodedJWT validatedToken = tokenService.validateToken(token, hostname);
+    @RequestMapping(value = "/auth", method = RequestMethod.POST)
+    public Token checkToken(@RequestBody CheckTokenRequestBody checkTokenRequestBody) throws MalformedURLException, AuthenticationException, JwkException {
+        final DecodedJWT validatedToken = tokenService.validateToken(
+                checkTokenRequestBody.getToken(),
+                checkTokenRequestBody.getHostname()
+        );
         final List<Role> roleList = tokenService.createRoleList(validatedToken);
         return new Token(
                 validatedToken.getClaim("preferred_username").asString(),
